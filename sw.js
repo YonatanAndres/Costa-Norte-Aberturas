4d12b933ea2190035e5e6cfbb1d5d2f76f865ef8
YonatanAndres <costanorteaberturas@gmail.com>
2026-08-24 19:25:08 -0300

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'cna-v2';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('./index.html'))
+  );
+});
